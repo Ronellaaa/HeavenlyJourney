6dfbed9691d7aa54993b6e42f1b07a2a9415bac7
Ronella Dias <ronelladias17@gmail.com>
2025-01-24 13:53:41 +0530

    did some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
      <Router>
             <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/mary" element={<MarianDevotion />} />
             </Routes>
         </Router>
